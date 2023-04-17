cf509d283dfe2488d2322d11e1bea6a57cb7946f
Marcelo Biotto <lee.am@live.com>
2023-04-17 05:54:20 -0300

    adicionando a pasta js

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const robotron = document.querySelector("#robotron")
+
+
+robotron.addEventListener("click", function(){
+    console.log("Cliquei no robo")
+} )
+
+function dizOi(){
+    console.log("oi");
+    console.log("Bem vindo ao Robotron 2000")
+}
+
+dizOi();
+
