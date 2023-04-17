d5bf96ab3245289e1e1c06310df3f4115154508e
Marcelo Biotto <lee.am@live.com>
2023-04-17 06:46:39 -0300

    adicionando os comandos de somar e subratir para os bracos

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
-const robotron = document.querySelector("#robotron")
+const subtrair = document.querySelector("#subtrair");
+const somar = document.querySelector("#somar");
+const braco = document.querySelector("#braco");
 
-
-robotron.addEventListener("click", function(){
-    console.log("Cliquei no robo")
+somar.addEventListener("click", (evento) =>{
+    braco.value = parseInt(braco.value) + 1 ;
 } )
 
-function dizOi(){
-    console.log("oi");
-    console.log("Bem vindo ao Robotron 2000")
-}
-
-dizOi();
+subtrair.addEventListener("click", (evento) =>{
+    braco.value = parseInt(braco.value) - 1 ;
+} )
 
